72a76fb4e0b4d73e30624aace6702b494a9ad19b
Ishaan vats <Ishaanvats74@gmail.com>
2025-07-22 04:25:53 +0530

    backend returning menu dataset

--- app/api/home/route.js
+++ app/api/home/route.js
@@ -1,15 +1,35 @@
-import { getDatabase, ref, onValue } from "firebase/database";
+
 import { initializeApp } from "firebase/app";
+import { NextResponse } from "next/server";
+import { doc, getDoc ,getFirestore} from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_KEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.NEXT_FIREBASE_APP_ID,
-  databaseURL: "https://asia-south2.firebaseio.com",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+export async function GET() {
+    const firebaseConfig = {
+        apiKey: process.env.NEXT_FIREBASE_KEY,
+        authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_FIREBASE_MESSAGE_SENDER_ID,
+        appId: process.env.NEXT_FIREBASE_APP_ID,
+        databaseURL: "https://asia-south2.firebaseio.com",
+    };
+    try {
+        
+        const app = initializeApp(firebaseConfig);
+        const db = getFirestore(app);
+        const q = query(collection(db, "menu"));
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((doc) => {
+            result = doc.data()
+            console.log(doc.id, " => ", doc.data());
+        });
+        
+        return NextResponse.json({result:result},{status:200})
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({error:error},{status:500})
+    }
+        
+    
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,24 +1,38 @@
 'use client';
 import { Switch } from "@/components/ui/switch"
+import { useEffect } from "react";
 
 
 export default function Home() {
-  const data = ()=>{
-    const fetchdata = async()=>{
-
+  useEffect(()=>{
+    try {
+      const fetchdata = async()=>{
+        await fetch("/api/home",{
+          method:"GET",
+          
+        })
+      }
+      fetchdata()
+      console.log(body)
+    } catch (error) {
+      alert(error)
     }
-  }
+  })
   return (
     <div className="max-h-full flex flex-col items-center ">
-      <div className="flex flex-col   border-black border-2 max-w-60">
-        <div className="  flex items-center justify-between m-2">
-          <p className="text-2xl font-semibold">Samosa</p>
-          <p className="bg-green-500 text-white px-1 text-sm ">In stock</p>
+      <div className=" grid grid-cols-2 gap-10">
+
+        <div className="flex flex-col shadow-lg rounded-lg p-2">
+          <div className="space-x-30  flex items-center justify-between m-2">
+            <p className="text-2xl font-semibold">Samosa</p>
+            <p className="bg-green-500 font-semibold text-white px-2 py-1 text-sm ">In stock</p>
+          </div>
+          <div className="flex justify-between m-2 space-x-30 ">
+            <p className="text-xs text-gray-400">Notify me on this item</p>
+            <Switch className={"h-5 "}/>
+          </div>
         </div>
-        <div className="flex justify-between m-2 gap-5">
-          <p className="text-sm">Notify me on this item</p>
-          <Switch />
-        </div>
+
       </div>
     </div>
   );
